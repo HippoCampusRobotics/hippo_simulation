31cd6aa24db577ffc724d484bb326c398385403e
Thies Lennart Alff <lennart.alff@gmail.com>
2024-05-23 09:31:33 +0000

    working kinematic loop test

---
+++ hippo_sim/launch/test_gripper.launch.py
@@ -0,0 +1,55 @@
+from simple_launch import GazeboBridge, SimpleLauncher
+
+sl = SimpleLauncher(use_sim_time=True)
+sl.declare_gazebo_axes(pitch=0.0)
+
+
+def launch_setup():
+    print('hello')
+    sl.gz_launch('empty.sdf')
+    sl.spawn_gz_model(
+        name='hippocampus',
+        model_file=sl.find('hippo_sim', 'gripper.urdf'),
+        spawn_args=sl.gazebo_axes_args(),
+    )
+    print('hello2')
+
+    bridges = [
+        GazeboBridge.clock(),
+    ]
+    print('hello 2.05')
+    # gz_topic = GazeboBridge.model_prefix('test')
+    gz_topic = '/world/empty/model/hippocampus'
+    print('hello 2.1')
+    bridges.append(
+        GazeboBridge(
+            gz_topic,
+            'joint_states',
+            'sensor_msgs/JointState',
+            GazeboBridge.gz2ros,
+        )
+    )
+    print('hello 2.5')
+    joints = [
+        'prismatic_joint',
+        'left_prismatic_revolute_joint',
+        'left_revolute_joint',
+    ]
+    print('hello3')
+    for i, joint in enumerate(joints):
+        print(i)
+        bridges.append(
+            GazeboBridge(
+                f'/model/hippocampus/joint/{joint}/cmd_force',
+                f'joint{i+1}_cmd_effort',
+                'std_msgs/Float64',
+                GazeboBridge.ros2gz,
+            )
+        )
+
+    sl.create_gz_bridge(bridges)
+    sl.node('slider_publisher', arguments=[sl.find('hippo_sim', 'test.yaml')])
+    return sl.launch_description()
+
+
+generate_launch_description = sl.launch_description(launch_setup)
